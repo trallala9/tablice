3b9e26fff61cf593ae5e7d376e74fd4691827966
Karolina Siwkowska <trallala00@gmail.com>
2017-12-30 22:28:53 +0100

    Init project

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var allNames = femaleNames.concat(maleNames);
+console.log(allNames);
+
+var newName = 'Marian'
+if (allNames.indexOf(newName) === -1);
+var x = allNames.push('newName');
